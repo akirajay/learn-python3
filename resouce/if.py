35320827d54f5422167f911c97c6993c3a325430
akirajay <wuliangjay@gmail.com>
2015-08-05 16:03:01 +0900

    upload

---
+++ resouce/if.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+#Filename:if.py
+
+number=23
+guss=int(input('Enter an integer'))
+
+if guss==number:
+    print ('Con,you guessed it')
+elif guss < number:
+    print ('No,it is a little higher than that')
+else:
+    print ('No,it is a little lower than that')
+print ('Done')
+
+
+ b'\xe4\xb8\xad\xe6\x96\x87'.decode('utf-8')
